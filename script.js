6f5eac13c38917f75e37347f0df211a15480635c
Pravin Rathod <medicogayatri3@gmail.com>
2026-03-15 14:20:30 +0530

    understanding about changing the color randomly from js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-let h1 = document.querySelector("h1")
-let inc = document.querySelector("#inc")
-let dec = document.querySelector("#dec")
+let box = document.querySelector("#box")
+let btn = document.querySelector("button")
 
-let a = 0
-inc.addEventListener("click",function(){
-    a++
-    h1.innerHTML=a
-})
-dec.addEventListener("click",function(){
-    a--
-    h1.innerHTML=a
+
+
+btn.addEventListener("click", function () {
+    let a = Math.floor(Math.random() * 256)
+    let b = Math.floor(Math.random() * 256)
+    let c = Math.floor(Math.random() * 256)
+    
+    box.style.backgroundColor = `rgb(${a},${b},${c})`
 })
