542bdf1895a491df5a791370cac4369236caea3f
Pravin Rathod <medicogayatri3@gmail.com>
2026-03-15 16:07:40 +0530

    lecture 43 task completed

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-let box = document.querySelector("#box")
+let ipl = [
+    { team: "CSK", primary: "blue", secondary: "black" },
+    { team: "DC", primary: "navy", secondary: "yellow" },
+    { team: "GT", primary: "gold", secondary: "black" },
+    { team: "KKR", primary: "purple", secondary: "gold" },
+    { team: "LSG", primary: "red", secondary: "orange" },
+    { team: "MI", primary: "navy", secondary: "yellow" },
+    { team: "PBKS", primary: "red", secondary: "gold" },
+    { team: "RCB", primary: "gold", secondary: "red" },
+    { team: "SRH", primary: "orange", secondary: "red" },
+    { team: "RR", primary: "magenta", secondary: "beige" }
+]
+
 let btn = document.querySelector("button")
-
-
+let h1 = document.querySelector("h1")
+let main = document.querySelector("main")
 
 btn.addEventListener("click", function () {
-    let a = Math.floor(Math.random() * 256)
-    let b = Math.floor(Math.random() * 256)
-    let c = Math.floor(Math.random() * 256)
+    let win = ipl[Math.floor(Math.random() * ipl.length)]
     
-    box.style.backgroundColor = `rgb(${a},${b},${c})`
+    h1.innerHTML = win.team
+    h1.style.backgroundColor = win.primary
+    main.style.backgroundColor = win.secondary
+
 })
