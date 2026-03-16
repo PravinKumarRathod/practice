0516f867ded80a798f3e7d63c897f84d0e2345e4
Pravin Rathod <medicogayatri3@gmail.com>
2026-03-16 23:42:36 +0530

    lecture 44 task completed

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
-let ipl = [
-    { team: "CSK", primary: "blue", secondary: "black" },
-    { team: "DC", primary: "navy", secondary: "yellow" },
-    { team: "GT", primary: "gold", secondary: "black" },
-    { team: "KKR", primary: "purple", secondary: "gold" },
-    { team: "LSG", primary: "red", secondary: "orange" },
-    { team: "MI", primary: "navy", secondary: "yellow" },
-    { team: "PBKS", primary: "red", secondary: "gold" },
-    { team: "RCB", primary: "gold", secondary: "red" },
-    { team: "SRH", primary: "orange", secondary: "red" },
-    { team: "RR", primary: "magenta", secondary: "beige" }
-]
-
 let btn = document.querySelector("button")
-let h1 = document.querySelector("h1")
 let main = document.querySelector("main")
+let hval = ["Blame plaused","Not commited yet","I am the worst","Stop procrastination right now","start reading books!!","Stop skiping classes","My head is hurting like hell"]
 
-btn.addEventListener("click", function () {
-    let win = ipl[Math.floor(Math.random() * ipl.length)]
+btn.addEventListener("click",function(){
+    let h1 = document.createElement("h1")
+
+    let a = Math.floor(Math.random()*hval.length)
     
-    h1.innerHTML = win.team
-    h1.style.backgroundColor = win.primary
-    main.style.backgroundColor = win.secondary
+    h1.innerHTML = hval[a]
 
-})
+    h1.style.position = 'absolute'
+    let x = Math.random()*80
+    let y = Math.random()*80
+    let rot = Math.floor(Math.random()*361)
+    h1.style.left = x+"%"
+    h1.style.top = y+"%"
+    h1.style.rotate = rot+"deg"
+
+    let zizi = Math.random()*2
+    h1.style.scale = zizi
+    // h1.style.position = x+"%"
+    // h1.style.position. = y+"%"
+    let c1 = Math.floor(Math.random()*256)
+    let c2 = Math.floor(Math.random()*256)
+    let c3 = Math.floor(Math.random()*256)
+    h1.style.color = `rgb(${c1},${c2},${c3})`
+
+    main.appendChild(h1)
+    })
+
