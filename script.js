faa943b96f3d755619c50907567a204a04c3a1d5
Pravin Rathod <medicogayatri3@gmail.com>
2026-03-17 23:08:51 +0530

    lecture 45 task completed of making a downloader

--- script.js
+++ script.js
@@ -1,31 +1,23 @@
 let btn = document.querySelector("button")
-let main = document.querySelector("main")
-let hval = ["Blame plaused","Not commited yet","I am the worst","Stop procrastination right now","start reading books!!","Stop skiping classes","My head is hurting like hell"]
+let h2 = document.querySelector("h2")
+let inner = document.querySelector("#inner")
+let grow = 0
 
 btn.addEventListener("click",function(){
-    let h1 = document.createElement("h1")
 
-    let a = Math.floor(Math.random()*hval.length)
+    let time = 50 + Math.floor(Math.random()*50)
+    btn.style.pointerEvents = "none"
+
+    let inter = setInterval(function(){
+        grow++
+        h2.innerHTML = grow+"%"
+        inner.style.width = grow+"%"
+    },time);
+    setTimeout(()=>{
+        clearInterval(inter)
+        btn.innerHTML = "Downloaded"
+        btn.style.opacity = 0.7
+        console.log("Downloaded in",time/10,"Seconds")
+    },time*100)
     
-    h1.innerHTML = hval[a]
-
-    h1.style.position = 'absolute'
-    let x = Math.random()*80
-    let y = Math.random()*80
-    let rot = Math.floor(Math.random()*361)
-    h1.style.left = x+"%"
-    h1.style.top = y+"%"
-    h1.style.rotate = rot+"deg"
-
-    let zizi = Math.random()*2
-    h1.style.scale = zizi
-    // h1.style.position = x+"%"
-    // h1.style.position. = y+"%"
-    let c1 = Math.floor(Math.random()*256)
-    let c2 = Math.floor(Math.random()*256)
-    let c3 = Math.floor(Math.random()*256)
-    h1.style.color = `rgb(${c1},${c2},${c3})`
-
-    main.appendChild(h1)
-    })
-
+})
